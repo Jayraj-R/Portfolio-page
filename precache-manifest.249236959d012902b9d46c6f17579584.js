80d22742c1f843ad40e1f3c72ca000b177a75bd4
Jayraj Rathod <jayrahod5@gmail.com>
2021-05-08 22:21:45 +0530

    Updates

---
+++ precache-manifest.249236959d012902b9d46c6f17579584.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3061ed2e9cc5b85a39d2590a7e0d12",
+    "url": "/Portfolio-page/index.html"
+  },
+  {
+    "revision": "b05a792a145575d72b7c",
+    "url": "/Portfolio-page/static/css/main.e33bcdb7.chunk.css"
+  },
+  {
+    "revision": "bbacefa7a69afa16a9a6",
+    "url": "/Portfolio-page/static/js/2.d3194604.chunk.js"
+  },
+  {
+    "revision": "84f7a781b6806de5124f",
+    "url": "/Portfolio-page/static/js/3.5fb5517d.chunk.js"
+  },
+  {
+    "revision": "b05a792a145575d72b7c",
+    "url": "/Portfolio-page/static/js/main.f30e4531.chunk.js"
+  },
+  {
+    "revision": "c941299e1c7ad1b3f7f2",
+    "url": "/Portfolio-page/static/js/runtime~main.e3cb3174.js"
+  },
+  {
+    "revision": "92ecfcd6f2ba9b2f9f9d45228a5eac81",
+    "url": "/Portfolio-page/static/media/phc.92ecfcd6.jpg"
+  },
+  {
+    "revision": "b551fea6581aef4e721f209249b831db",
+    "url": "/Portfolio-page/static/media/ttt.b551fea6.png"
+  },
+  {
+    "revision": "eb30ce00396688daf51a6eb65650e727",
+    "url": "/Portfolio-page/static/media/weather.eb30ce00.jpg"
+  }
+]);
